10765d1c8b2c5579933da1384268c9ff569a2b5f
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-05 23:37:07 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -6,9 +6,14 @@ def third_challenge
         age: "53"
         },
       :matriarch => {
+        name: "Lady Montague",
+        age: "54"
         #your key/value pairs here
         },
       :hero => {
+        :name => "Romeo",
+        age: "15"
+        :status => alive
         #your key/value pairs here
         },
       :hero_friends => []
